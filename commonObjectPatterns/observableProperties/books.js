d6a88b9d2ab82d95df374590fe2d8d5dfee815ef
Ryan Kramer <RKramer613@gmail.com>
2017-06-29 16:17:17 -0700

    Created book script to see how methods and properties work

--- commonObjectPatterns/observableProperties/books.js
+++ commonObjectPatterns/observableProperties/books.js
@@ -1,3 +1,42 @@
 
 // This shows us how to use "Methods as Properties" design pattern
 
+var Book = function (name, price) {
+    this.name = function (val) {
+
+    };
+
+    this.price = function (val) {
+
+    };
+
+    this.onPriceChanging = function (callback) {
+
+    };
+
+    this.onPriceChanged = function (callback) {
+
+    };
+};
+
+var book = new Book('JavaScript: The Good Parts', 23.99);
+
+console.log('The name is: ' + book.name());
+
+console.log('The price is: $' + book.price());
+
+book.onPriceChanging(function(b, price) {
+    if(price > 100) {
+        console.log('System error, price has gone unexpectantly high');
+        return false;
+    }
+    return true;
+});
+
+book.onPriceChanged(function (b) {
+    console.log('The book price has changed to: $' + b.price());
+});
+
+book.price(19.99);
+
+book.price(200);

4265fdaeb6c2f97aa4f6642d058392a6d1235114
Ryan Kramer <RKramer613@gmail.com>
2017-06-29 15:28:14 -0700

    changed the value of this to equal that

--- commonObjectPatterns/chaining/calc.js
+++ commonObjectPatterns/chaining/calc.js
@@ -1,17 +1,18 @@
 var Calc = function (start) {
+    var that = this;
     this.add = function(x) {
         start = start + x;
-        return this;
+        return that;
     };
 
     this.multiply = function(x) {
         start = start * x;
-        return this;
+        return that;
     };
 
     this.equals = function(callback) {
         callback(start);
-        return this;
+        return that;
     };
 }
 

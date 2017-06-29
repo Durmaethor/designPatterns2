f3bae07b07609b27bde75bc2bf0d99263f533d27
Ryan Kramer <RKramer613@gmail.com>
2017-06-29 15:37:08 -0700

    Function Arguments folder and file added. Code complete and tested.

---
+++ functionArgs/funcArgs.js
@@ -0,0 +1,18 @@
+
+//
+
+function myFunc(a,b,c) {
+    return a + b + c;
+}
+
+console.log(myFunc(1,2,3)); // This will return exactly as it is supposed to
+console.log(myFunc(1,2,3,4)); // This will return exactly as it is supposed to, completely ignoring the 4
+console.log(myFunc(1,2)); // This will cause an error as there is no 'c', and it will show NaN
+
+// Results 
+
+        // 6
+        // 6
+        // NaN
+
+        

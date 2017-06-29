d1bf0986f2689ecc8ada1a699ac852b860dfce4f
Ryan Kramer <RKramer613@gmail.com>
2017-06-29 15:44:04 -0700

    new function with look to pass in arg obj

--- functionArgs/funcArgs.js
+++ functionArgs/funcArgs.js
@@ -17,3 +17,12 @@ console.log(myFunc(1,2)); // This will cause an error as there is no 'c', and it
 
 // To solve this possible issue, we can use an Argument Object
 
+function myFunc() {
+    var x = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        x = x + arguments[i];
+    }
+    return x;
+}
+
+// This simply passes all of the parameters through the argument object, making us not need to pass things through with a, b, c, etc.

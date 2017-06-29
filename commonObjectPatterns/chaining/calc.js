09a1d79c8b09c68e4e3ae1a9f0a03c88699ed9a5
Ryan Kramer <RKramer613@gmail.com>
2017-06-29 15:26:04 -0700

    Added notes and implementing method bodies

--- commonObjectPatterns/chaining/calc.js
+++ commonObjectPatterns/chaining/calc.js
@@ -1,14 +1,17 @@
 var Calc = function (start) {
     this.add = function(x) {
-
+        start = start + x;
+        return this;
     };
 
     this.multiply = function(x) {
-
+        start = start * x;
+        return this;
     };
 
     this.equals = function(callback) {
-
+        callback(start);
+        return this;
     };
 }
 
@@ -19,3 +22,5 @@ new Calc(0)
     .equals(function(result) {
         console.log(result)
     });
+
+    // This: represents the current state which the method was invoked off of; meaning that when it's the chained API, the first time we call add, the this value will be the calculator.
